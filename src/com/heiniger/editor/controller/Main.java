c72d4d4fb434b125fee88a3e6c8163ec44d348c1
Daniel <runaway@Daniel.(none)>
2014-06-13 23:06:36 -0500

    Initial Commit

---
+++ src/com/heiniger/editor/controller/Main.java
@@ -0,0 +1,7 @@
+package com.heiniger.editor.controller;
+
+public class Main {
+	public static void main(String[] args){
+		
+	}
+}
---
+++ src/com/heiniger/editor/view/GUI.java
@@ -0,0 +1,5 @@
+package com.heiniger.editor.view;
+
+public class GUI {
+
+}
