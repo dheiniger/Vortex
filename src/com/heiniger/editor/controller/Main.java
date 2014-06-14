a557d669e2c57a74015b748fdba0bff5cf3a70a0
Daniel <runaway@Daniel.(none)>
2014-06-14 01:11:43 -0500

    Create initial editor user interface

--- src/com/heiniger/editor/controller/Main.java
+++ src/com/heiniger/editor/controller/Main.java
@@ -1,7 +1,9 @@
 package com.heiniger.editor.controller;
 
+import com.heiniger.editor.view.GUI;
+
 public class Main {
 	public static void main(String[] args){
-		
+		new GUI();
 	}
 }
--- src/com/heiniger/editor/view/GUI.java
+++ src/com/heiniger/editor/view/GUI.java
@@ -1,5 +1,36 @@
 package com.heiniger.editor.view;
 
-public class GUI {
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.border.BevelBorder;
+import javax.swing.JButton;
 
+
+public class GUI {
+	private JTable editTable;
+	private JTable selectTable;
+	private JButton btnGenerateXml;
+	public GUI(){
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(1200, 800);
+		window.setVisible(true);
+		window.setTitle("Level Editor");		
+		window.getContentPane().setLayout(null);		
+
+		selectTable = new JTable();
+		selectTable.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		selectTable.setBounds(39, 35, 220, 499);
+		
+		editTable = new JTable();
+		editTable.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		editTable.setBounds(304, 35, 857, 499);
+		
+		window.getContentPane().add(editTable);
+		window.getContentPane().add(selectTable);
+		
+		btnGenerateXml = new JButton("Generate XML");
+		btnGenerateXml.setBounds(304, 612, 134, 25);
+		window.getContentPane().add(btnGenerateXml);
+	}
 }
